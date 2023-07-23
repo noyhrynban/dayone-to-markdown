9d77ce917ca9d48b71f92f84cbf80e5001e21c57
Ryan Harper <36581272+noyhrynban@users.noreply.github.com>
2023-07-23 11:01:36 +0900

    output entry text
    replace weirdly escaped characters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use serde::Deserialize;
+use std::{
+    fs::{self},
+    path::Path,
+    process::exit,
+};
+
+#[derive(Debug, Deserialize)]
+struct Entry {
+    text: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Journal {
+    entries: Vec<Entry>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let arg = match args.get(1) {
+        Some(arg) => arg,
+        None => {
+            println!("No path was provided. Please provide a path to the journal JSON file.");
+            exit(1)
+        }
+    };
+    let file_path: &Path = Path::new(arg);
+    if !file_path.exists() {
+        println!("Path does not exist.");
+        exit(1)
+    }
+
+    let contents: String = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(e) => panic!("{e}"),
+    };
+
+    let journal: Journal = match serde_json::from_str(&contents) {
+        Ok(journal) => journal,
+        Err(e) => panic!("{e}"),
+    };
+    for entry in journal.entries {
+        println!("{}", cleanup(entry.text).as_str());
+    }
+}
+
+fn cleanup(string: String) -> String {
+    string
+        .replace(r"\.", ".")
+        .replace(r"\(", "(")
+        .replace(r"\)", ")")
+        .replace(r"\!", "!")
+        .replace(r"\-", "-")
+        .replace(r"\+", "+")
+        .replace(r"\[", "[")
+        .replace(r"\]", "]")
 }
